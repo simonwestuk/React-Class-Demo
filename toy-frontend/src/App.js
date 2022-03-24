2b7144b8a802696f5075780d5a6274afbc79ae86
wests <wests@C-010053>
2022-03-24 15:58:29 +0000

    Added Login

--- toy-frontend/src/App.js
+++ toy-frontend/src/App.js
@@ -15,7 +15,7 @@ import SingleProductScreen from './Screens/SingleProductScreen';
 import ManageProductsScreen from './Screens/ManageProductsScreen';
 import EditProductScreen from './Screens/EditProductScreen';
 import RegisterScreen from './Screens/RegisterScreen';
-
+import LoginScreen from './Screens/LoginScreen';
 //import routing for page navigation
 import {
   BrowserRouter as Router,
@@ -32,14 +32,12 @@ function App() {
           <Routes>
             <Route exact path="/" element={<HomeScreen/>} />
             <Route exact path ="/products" element={<ProductScreen/>}/>
-
             <Route exact path ="/products/:id" element={<SingleProductScreen/>}/>
-
             <Route exact path ="/locations" element={<LocationsScreen/>}/>
             <Route exact path ="/about" element={<AboutScreen/>}/>
             <Route exact path ="/contact" element={<ContactScreen/>}/>
             <Route exact path ="/register" element={<RegisterScreen/>}/>
-
+            <Route exact path ="/login" element={<LoginScreen/>}/>
             <Route exact path = "/admin/addproduct" element={<AddProductScreen/>}/>
             <Route exact path = "/admin/manageproducts" element={<ManageProductsScreen/>}/>
             <Route exact path = "/admin/product/edit/:id" element={<EditProductScreen/>}/>
